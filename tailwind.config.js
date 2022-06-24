ddfaaaec5726f337462e6087544cd49854aee2c5
ifGabrielCode <jgfinacio@gmail.com>
2022-06-24 19:06:43 -0300

    commit taiwind aprendizado

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html"
+
+  ],
+  theme: {
+    extend: {
+      backgroundColor:{
+        home: "#ff0000"
+      }
+    },
+  },
+  plugins: [],
+}
